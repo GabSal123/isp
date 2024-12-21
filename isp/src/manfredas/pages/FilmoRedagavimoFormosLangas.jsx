efda96151ce869ffeb895e70f8e7043e1c9a3274
ManfredasLamsargis <manfredas.lamsargis@proton.me>
2024-12-21 19:48:15 +0200

    Labai paprastas filmo redagavimo formos langas

--- isp/src/manfredas/pages/FilmoRedagavimoFormosLangas.jsx
+++ isp/src/manfredas/pages/FilmoRedagavimoFormosLangas.jsx
@@ -3,6 +3,42 @@ import { useNavigate } from 'react-router-dom';
 import { useParams } from 'react-router-dom';
 import axios from 'axios';
 
+function textInputArea(text) {
+    return (
+        <div>
+            <textarea type="text" defaultValue={text} style={{display: 'flex'}}></textarea>
+        </div>
+    );
+}
+
+function displayMovieEditingForm(movie) {
+    return (
+        <div
+            key={movie.id}
+            style={{
+                display: 'flex',
+                    justifyContent: 'center',
+                    alignItems: 'center',
+                    flexDirection: 'column',
+                    gap: '20px',
+            }}
+        >
+            <h1>Filmo redagavimas</h1>
+            {textInputArea(movie.title)}
+            {textInputArea(movie.startingFrom)}
+            {textInputArea(movie.showingUntil)}
+            {textInputArea(movie.cover)}
+            {textInputArea(movie.isDubbed)}
+            {textInputArea(movie.subtitles)}
+            {textInputArea(movie.description)}
+            {textInputArea(movie.trailerLink)}
+            {textInputArea(movie.duration)}
+            {textInputArea(movie.studio)}
+            {textInputArea(movie.language)}
+            {textInputArea(movie.ageCensorship)}
+        </div>
+    )
+}
 
 function FilmoRedagavimoFormosLangas() {
     const { id } = useParams();
@@ -18,7 +54,9 @@ function FilmoRedagavimoFormosLangas() {
     },[]);
 
     return (
-        <h1>{movie.title} Hello, world!</h1>
+        <div>
+            {displayMovieEditingForm(movie)}
+        </div>
     );
 }
 
