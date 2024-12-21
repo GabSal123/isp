179aaad3c659cd44f8c4514fe4a8f1e338402e73
ManfredasLamsargis <manfredas.lamsargis@proton.me>
2024-12-21 15:01:43 +0200

    Paprastas filmo displayinimas pagrindiniame filmu lange

--- isp/src/manfredas/pages/PagrindinisFilmuLangas.jsx
+++ isp/src/manfredas/pages/PagrindinisFilmuLangas.jsx
@@ -2,16 +2,31 @@ import { useEffect, useState } from 'react'
 import { useNavigate } from 'react-router-dom';
 import { useParams } from 'react-router-dom';
 import axios from 'axios';
-import FilmoVertinimoFormosLangas from './FilmoVertinimoFormosLangas';
+
+
+function displayMovie(movie) {
+    return (
+        <div key={movie.id}>
+            <h2>{movie.title}</h2>
+            <img src={`/src/manfredas/assets/movie_covers/${movie.title}.jpg`}/>
+            <p>{movie.description}</p>
+        </div>
+    );
+}
+
 
 function PagrindinisFilmuLangas() {
     const [movieList, setMovieList] = useState([])
 
     useEffect(()=>{
         axios.get(`https://localhost:7241/GetAllMovies`)
-        .then((result)=>{console.log(result)})
+            .then((result)=>{
+                setMovieList(result.data);
+                console.log(result);
+            })
     }, [])
 
+    
     const navigate = useNavigate();
     const navigateToFilmoLangas = () => {
         navigate(`/FilmoLangas`);
@@ -19,7 +34,12 @@ function PagrindinisFilmuLangas() {
 
     return (
         <div>
-            Pagrindinis filmų langas
+            <h1>
+                Pagrindinis filmų langas
+            </h1>
+            <div>
+               {movieList.map((movie) => displayMovie(movie))}
+            </div>
             <br></br>
             <button onClick={navigateToFilmoLangas}>I filmo langa</button>
         </div>
